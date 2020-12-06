de372779dacd1c34eb4b724be2c10a3e7c676435
PssbleTrngle <nik@somethingcatchy.net>
2020-12-06 23:05:28 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+# 1.16.4
---
+++ src/link.bat
@@ -0,0 +1,6 @@
+mklink /J generated/data/floating/recipes "main\resources\data\floating\recipes"
+mklink /J generated/data/floating/loot_tables "main\resources\data\floating\loot_tables"
+mklink /J generated/data/floating/advancements "main\resources\data\floating\advancements"
+
+mklink /J generated/assets/floating/models "main\resources\assets\floating\models"
+mklink /J generated/assets/floating/blockstates "main\resources\assets\floating\blockstates"
